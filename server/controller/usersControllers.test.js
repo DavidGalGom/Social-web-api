64b0116bb9092208d1f73c21d309c220db33aae0
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-13 15:52:39 +0100

    Added test for userController with right user and wrong password

--- server/controller/usersControllers.test.js
+++ server/controller/usersControllers.test.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../../database/models/user");
 const { checkUser } = require("./usersControllers");
 
@@ -19,6 +21,30 @@ describe("Given a User", () => {
       const next = jest.fn();
       const expectedError = new Error("Authentication failed");
       expectedError.code = 401;
+
+      await checkUser(req, null, next);
+
+      expect(next.mock.calls[0][0]).toHaveProperty("message");
+      expect(next.mock.calls[0][0]).toHaveProperty("code", 401);
+    });
+  });
+
+  describe("When it receives a correct username and a wrong password", () => {
+    test("Then it should summon the function checkUser with an error and a 401 code", async () => {
+      const req = {
+        body: {
+          username: "Davidgg",
+          password: "1234abcd",
+          name: "David",
+          photo: "qwe",
+          bio: "qwe",
+        },
+      };
+      User.findOne = jest.fn().mockResolvedValue(req.body.username);
+      const next = jest.fn();
+      const expectedError = new Error("Authentication failed");
+      expectedError.code = 401;
+      bcrypt.compare = jest.fn().mockResolvedValue(false);
 
       await checkUser(req, null, next);
 
