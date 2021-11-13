c3eaae0512c7e8c1d412ae22521295c79f2b64fd
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-13 19:54:30 +0100

    Fixed some validation requirements

--- server/schemas/userSchema.js
+++ server/schemas/userSchema.js
@@ -3,9 +3,7 @@ const { Joi } = require("express-validation");
 const userValidation = {
   body: Joi.object({
     username: Joi.string().required(),
-    password: Joi.string()
-      .regex(/[a-zA-Z0-9]{3,30}/)
-      .required(),
+    password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/),
     name: Joi.string().required(),
     image: Joi.string().required(),
     bio: Joi.string().required(),
