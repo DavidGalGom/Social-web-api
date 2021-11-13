836a5acb6e47dae7b0978434b15399f905913cb8
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-13 16:41:24 +0100

    Fixed the reverted changes of a wrong revert commit

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { validate } = require("express-validation");
 const { checkUser, getUsers } = require("../controller/usersControllers");
+const userValidation = require("../schemas/userSchema");
 
 const router = express.Router();
 
 router.post("/", checkUser);
-router.get("/", getUsers);
+router.get("/", validate(userValidation, {}, {}), getUsers);
 
 module.exports = router;
