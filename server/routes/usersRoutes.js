4e9d1849480d3c72e208d782b325df6defc5ac68
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-13 13:13:47 +0100

    Added userCreate with bcrypt pass

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { validate } = require("express-validation");
 const { checkUser, getUsers } = require("../controller/usersControllers");
+const userValidation = require("../schemas/userSchema");
 
 const router = express.Router();
 
-router.post("/", checkUser);
+router.post("/", validate(userValidation, {}, {}), checkUser);
 router.get("/", getUsers);
 
 module.exports = router;
